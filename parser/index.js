cedc04655a525938afd202b4005671d1850ef79f
dicristomanuel <manuel.dicristo@raise.com>
2016-08-01 20:41:56 -0500

    [parseNounDefinition] create

--- parser/index.js
+++ parser/index.js
@@ -3,21 +3,23 @@ import _ from 'lodash';
 export default (result) => {
 
   let concepts = [];
+  let currentVerb;
+  let currentNoun;
 
   const toSentence = result.document.sentences.sentence
   const sentence = toSentence[0];
 
-  const getWord = (element) => {
+  const parseNounDefinition = (element) => {
     let depth = 0;
     if (_.has(element, 'children')) {
       depth = element.children.length;
       for (let i = 0; i <= depth - 1; i++) {
-        getWord([element.children[i]])
+        parseNounDefinition([element.children[i]])
       }
     } else if (_.has(element[0], 'children')) {
       depth = element[0].children.length;
       for (let i = 0; i <= depth - 1; i++) {
-        getWord([element[0].children[i]])
+        parseNounDefinition([element[0].children[i]])
       }
     } else {
       concepts[concepts.length - 1].phrase.push(element[0].word);
@@ -55,7 +57,8 @@ export default (result) => {
     if (hasPattern) {
       hasPattern.segments.forEach((segment) => {
         concepts.push({ type: segment.type, phrase: [] })
-        getWordFor(segment);
+        parseNounDefinition(segment);
+        // when promise parseND ends set temp values for NP and VP
       })
     }
 
